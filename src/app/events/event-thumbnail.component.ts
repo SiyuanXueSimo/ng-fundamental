996f7d8c18b5b3a3a4f757471ab318f267077ada
Siyuan Xue <siyuan.xue@elekta.com>
2022-01-03 14:48:30 +0200

    ngIf practise

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,23 +4,26 @@ import { Component, Input} from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{vEvent?.name}}</h2>
+        <div>Date: {{vEvent?.date}}</div>
+        <div>Time: {{vEvent?.time}}</div>
+        <div>Price: \${{vEvent?.price}}</div>
+        <div *ngIf="vEvent?.location">
+            <span>Location: {{vEvent?.location?.address}}</span>
+            <span class="pad-left">{{vEvent?.location?.city}}, {{vEvent?.location?.country}}</span>
+        </div>
+        <div *ngIf="vEvent?.onlineUrl">
+            Online URL: {{vEvent?.onlineUrl}}
         </div>
     </div>
-    `
+    `,
+    styles: [`
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 
 export class EventThumbnailComponent {
-    @Input() event: any
-    someProperty:any = "some value"
-
-    logFoo() {
-        console.log("log foo");
-    }
+    @Input() vEvent: any
 }
