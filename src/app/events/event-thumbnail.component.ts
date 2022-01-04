f386120761cc054141e3776b41ef72912e542df3
Siyuan Xue <siyuan.xue@elekta.com>
2022-01-04 09:54:30 +0200

    Demo for ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input} from "@angular/core";
     <div class="well hoverwell thumbnail">
         <h2>{{vEvent?.name}}</h2>
         <div>Date: {{vEvent?.date}}</div>
-        <div>Time: {{vEvent?.time}}</div>
+        <div class = "well" [ngClass]="getStartTimeClass()" [ngSwitch]="vEvent?.time">
+            Time: {{vEvent?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
         <div>Price: \${{vEvent?.price}}</div>
         <div [hidden]="!vEvent?.location">
             <span>Location: {{vEvent?.location?.address}}</span>
@@ -18,6 +23,8 @@ import { Component, Input} from "@angular/core";
     </div>
     `,
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
         .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
@@ -26,4 +33,10 @@ import { Component, Input} from "@angular/core";
 
 export class EventThumbnailComponent {
     @Input() vEvent: any
+
+    getStartTimeClass() {
+        if(this.vEvent && this.vEvent.time == '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
 }
